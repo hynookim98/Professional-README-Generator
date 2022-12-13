042ff6ffad5fe4a747d72bbdee95a7b352ab2db2
hynookim98 <hynookim98@gmail.com>
2022-12-12 21:43:20 -0800

    test commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -58,7 +60,9 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile()
+}
 
 // TODO: Create a function to initialize app
 function init() {}
